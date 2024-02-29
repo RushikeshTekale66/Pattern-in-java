ac731fa4d3a2feef2d855ec15bc37e30900aced1
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-02-29 07:39:21 +0530

    Star Pattern 3 : Program to print mirrored right triangle star pattern..

--- Main.java
+++ Main.java
@@ -1,16 +1,14 @@
 class Main {
     void pattern() {
-        // print an inverted right triangle star pattern.
-        /*
-        *****
-        ****
-        ***
-        **
-        *
-         */
         int length = 5;
+
+        // Program to print mirrored right triangle star pattern..
+
         for (int i = 0; i < length; i++) {
             for (int j = length - i; j > 0; j--) {
+                System.out.print(".");
+            }
+            for (int k = 0; k <= i; k++) {
                 System.out.print("*");
             }
             System.out.println();
@@ -18,7 +16,7 @@ class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Star Pattern 2 : print an inverted right triangle star pattern.");
+        System.out.println("Star Pattern 3 : Program to print mirrored right triangle star pattern..");
         Main p = new Main();
         p.pattern();
     }
