b51b6877144bf7d72b053d7ba5ff563903824560
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-02-29 07:33:45 +0530

    Star Pattern 2 : print an inverted right triangle star pattern.

--- Main.java
+++ Main.java
@@ -1,19 +1,24 @@
 class Main {
-    void pattern(){
-        // print the right triangle star pattern.
-        /*  *
-            **
-            ***
-            ****
-            ***** */
-        for(int i=0; i<5; i++){
-            for(int j=0; j<=i; j++){
+    void pattern() {
+        // print an inverted right triangle star pattern.
+        /*
+        *****
+        ****
+        ***
+        **
+        *
+         */
+        int length = 5;
+        for (int i = 0; i < length; i++) {
+            for (int j = length - i; j > 0; j--) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
-    public static void main(String [] args){
+
+    public static void main(String[] args) {
+        System.out.println("Star Pattern 2 : print an inverted right triangle star pattern.");
         Main p = new Main();
         p.pattern();
     }
