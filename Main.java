5cdc759862517324f6cde8d2bdf21858a7e4d520
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-03-02 10:33:45 +0530

    Star Pattern 5 : Program to Display the Full Pyramid Star Pattern

--- Main.java
+++ Main.java
@@ -2,27 +2,27 @@ class Main {
     void pattern() {
         int length = 5;
 
-        // Program to print the Inverted Half Pyramid Star pattern.
+        // Display the Full Pyramid Star Pattern
         /*
-          .*****
-          ..****
-          ...***
-          ....**
-          .....*
+    * 
+   * *
+  * * *
+ * * * *
+* * * * *
          */
         for (int i = 0; i < length; i++) {
-            for (int k = 0; k <= i; k++) {
-                System.out.print(".");
+            for(int j=length-i; j>=0; j--){
+                System.out.print(" ");
             }
-            for (int j = i; j < length; j++) {
-                System.out.print("*");
+            for(int k=0; k<=i; k++){
+                System.out.print("* ");
             }
             System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        System.out.println("Star Pattern 4 : Program to print the Inverted Half Pyramid Star pattern.");
+        System.out.println("Star Pattern 5 : Program to Display the Full Pyramid Star Pattern");
         Main p = new Main();
         p.pattern();
     }
