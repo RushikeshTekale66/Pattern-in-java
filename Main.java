6cc0b1ffabac4441b58202e92b65bba7d4c79d1f
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-03-03 10:31:33 +0530

    Star Pattern 6 : Program to print inverted pyramid star pattern

--- Main.java
+++ Main.java
@@ -2,27 +2,28 @@ class Main {
     void pattern() {
         int length = 5;
 
-        // Display the Full Pyramid Star Pattern
+        // Program to print inverted pyramid star pattern
         /*
-    * 
-   * *
-  * * *
- * * * *
 * * * * *
+ * * * *
+  * * *
+   * *
+    *
          */
         for (int i = 0; i < length; i++) {
-            for(int j=length-i; j>=0; j--){
-                System.out.print(" ");
-            }
+            
             for(int k=0; k<=i; k++){
-                System.out.print("* ");
+                System.out.print(".");
             }
+            for(int j=i; j<length; j++){
+                System.out.print("* ");
+        }
             System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        System.out.println("Star Pattern 5 : Program to Display the Full Pyramid Star Pattern");
+        System.out.println("Star Pattern 6 : Program to print inverted pyramid star pattern");
         Main p = new Main();
         p.pattern();
     }
