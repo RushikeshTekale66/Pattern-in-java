fb0b7f5aa5d41ac692bceca44bd3ce03231b483c
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-03-05 20:14:35 +0530

    Star Pattern 8 : Program to print the Solid Half Diamond Star Pattern.

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main {
     void pattern() {
         int length = 5;
 
-        // Program to print square star pattern
+        //  Program to print the Solid Half Diamond Star Pattern.
         /*
 * * * * * 
 * * * * * 
@@ -12,16 +12,22 @@ class Main {
          */
         for (int i = 0; i < length; i++) {
             
-            for(int j=0; j<length; j++){
-                System.out.print("* ");
+            for(int j=0; j<=i; j++){
+                System.out.print("*");
             }
-            System.err.println();
-            
+            System.out.println();            
+        }
+        for(int i=0; i<length; i++){
+            for(int j=0; j<length-i; j++){
+                System.out.print("*");
+                
+            }
+            System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        System.out.println("Star Pattern 7 :Program to print square star pattern");
+        System.out.println("Star Pattern 8 : Program to print the Solid Half Diamond Star Pattern.");
         Main p = new Main();
         p.pattern();
     }
