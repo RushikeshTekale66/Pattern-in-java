4bda2135c0257c6529ff69ea1432a62b1e4472fe
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-03-05 20:05:46 +0530

    Star Pattern 7 :Program to print square star pattern

--- Main.java
+++ Main.java
@@ -2,28 +2,26 @@ class Main {
     void pattern() {
         int length = 5;
 
-        // Program to print square star pattern 
+        // Program to print square star pattern
         /*
+* * * * * 
+* * * * * 
 * * * * *
- * * * *
-  * * *
-   * *
-    *
+* * * * *
+* * * * *
          */
         for (int i = 0; i < length; i++) {
             
-            for(int k=0; k<=i; k++){
-                System.out.print(".");
-            }
-            for(int j=i; j<length; j++){
+            for(int j=0; j<length; j++){
                 System.out.print("* ");
-        }
-            System.out.println();
+            }
+            System.err.println();
+            
         }
     }
 
     public static void main(String[] args) {
-        System.out.println("Star Pattern 7 : Program to print square star pattern");
+        System.out.println("Star Pattern 7 :Program to print square star pattern");
         Main p = new Main();
         p.pattern();
     }
