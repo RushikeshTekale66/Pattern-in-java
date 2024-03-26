1f04469b00686bce62c2a177b7362eb633fbee9f
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-03-26 19:01:57 +0530

    Star Pattern 11 : Program to print the Full Pyramid Star Pattern.

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main {
     void pattern() {
         int length = 5;
 
-        //  Program to Program to print the Inverted V Star Pattern..
+        //  Program to print the Full Pyramid Star Pattern
         /*
             ....*.....
             ...*.*....
@@ -10,26 +10,23 @@ class Main {
             .*.....*..
             *.......*.
          */
-        int x = length;
-        int y = length;
-        for (int j = 1; j <= length; j++) {
-            for (int i = 1; i <= length * 2; i++) {
-                if (i == x || i == y) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(".");
-                }
-
+        int start = 1;
+        for(int i=length; i>0; i--){
+            for(int j=0; j<i; j++){
+                System.out.print(".");
             }
-            x--;
-            y++;
+            for(int k=0; k<start; k++){
+                System.out.print("*");
+                
+            }
+            start +=2;
             System.out.println();
         }
 
     }
 
     public static void main(String[] args) {
-        System.out.println("Star Pattern 10 :  Program to Program to print the Inverted V Star Pattern.");
+        System.out.println("Star Pattern 11 : Program to print the Full Pyramid Star Pattern.");
         Main p = new Main();
         p.pattern();
     }
