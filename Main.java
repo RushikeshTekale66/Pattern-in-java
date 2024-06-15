9efe18057c51ed73e3733d874df654915cd0e1d6
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-06-15 20:18:02 +0530

    Diamond Pattern

--- Main.java
+++ Main.java
@@ -1,24 +1,46 @@
 class Main {
     void pattern() {
         int length = 5;
-//          *# 
-//         * *# #
-//       * * *# # #
-//     * * * *# # # #
-//   * * * * *# # # # #
+//          * 
+//         * * #
+//       * * * # #
+//     * * * * # # #
+//   * * * * * # # # #
+// * * * * * * # # # # #
+// * * * * * * # # # # # #
+//   * * * * * # # # # #
+//     * * * * # # # #
+//       * * * # # #
+//         * * # #
+//           * #
         
         System.out.println("Hill trangle");
-        for(int i=1; i<=length; i++){
-            for(int j=1; j<=length-i; j++){
+        for(int i=0; i<=length; i++){
+            for(int j=0; j<=length-i; j++){
                 System.out.print("  ");
             }
             
-            for(int k=1; k<=i; k++){
-                System.out.print(" *");
+            for(int k=0; k<=i; k++){
+                System.out.print("* ");
             }
             for(int l=0; l<i; l++){
                 System.out.print("# ");
             }
+            System.out.println();
+        }
+
+
+        for(int i=0; i<=length; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print("  ");
+            }
+            for(int k=0; k<=length-i; k++){
+                System.out.print("* ");
+            }
+            for(int l=0; l<=length-i; l++){
+                System.out.print("# ");
+            }
+
             System.out.println();
         }
 
