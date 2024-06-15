01378b0bb8a0fc1054e29d5dc9ccc00c39ba4095
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-06-15 20:02:35 +0530

    Hill pattern

--- Main.java
+++ Main.java
@@ -1,21 +1,24 @@
 class Main {
     void pattern() {
         int length = 5;
-        // * * * *
-        //   * * *
-        //     * *
-        //       *
+//          *# 
+//         * *# #
+//       * * *# # #
+//     * * * *# # # #
+//   * * * * *# # # # #
         
-        System.out.println("Right side decreasing trangle");
+        System.out.println("Hill trangle");
         for(int i=1; i<=length; i++){
-            
-            for(int k=1; k<=i; k++){
+            for(int j=1; j<=length-i; j++){
                 System.out.print("  ");
             }
-            for(int j=1; j<=length-i; j++){
-                System.out.print("* ");
-            }
             
+            for(int k=1; k<=i; k++){
+                System.out.print(" *");
+            }
+            for(int l=0; l<i; l++){
+                System.out.print("# ");
+            }
             System.out.println();
         }
 
