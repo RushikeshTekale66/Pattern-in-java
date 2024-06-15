ff6850e83a56f9b7e802dcdd03d8a84b630f4933
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-06-15 19:58:09 +0530

    Right side decreasing triangle

--- Main.java
+++ Main.java
@@ -1,19 +1,19 @@
 class Main {
     void pattern() {
         int length = 5;
-//           #
-//         # #
-//       # # #
-//     # # # #
-//   # # # # #
+        // * * * *
+        //   * * *
+        //     * *
+        //       *
         
-        System.out.println("Right side trangle");
+        System.out.println("Right side decreasing trangle");
         for(int i=1; i<=length; i++){
-            for(int j=1; j<=length-i; j++){
+            
+            for(int k=1; k<=i; k++){
                 System.out.print("  ");
             }
-            for(int k=1; k<=i; k++){
-                System.out.print("# ");
+            for(int j=1; j<=length-i; j++){
+                System.out.print("* ");
             }
             
             System.out.println();
